9a70d92f21aca98384628259233f2277bb4e0459
Amir Raminfar <findamir@gmail.com>
2024-06-08 18:52:18 -0700

    adds char data change

--- src/content.ts
+++ src/content.ts
@@ -51,4 +51,4 @@ const observer = new MutationObserver((mutations) => {
   });
 });
 
-observer.observe(document.body, { childList: true, subtree: true });
+observer.observe(document.body, { childList: true, subtree: true, characterData: true});
