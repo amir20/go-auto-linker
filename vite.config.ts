5afc0d6c433f833f8218c8cd3a45533921eaef1b
Amir Raminfar <findamir@gmail.com>
2024-06-10 08:46:34 -0700

    updates readme

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ function generateManifest() {
   const manifest = readJsonFile("src/manifest.json");
   const pkg = readJsonFile("package.json");
   return {
-    name: "Go Auto Link",
+    name: "Go Auto Linker",
     description: pkg.description,
     version: pkg.version,
     ...manifest,
